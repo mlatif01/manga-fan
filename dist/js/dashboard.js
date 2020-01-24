65d8a26cd609027f5275b1c677b88421a831f158
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-01-24 16:41:33 +0000

    add modal popup for adding manga

--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -13,17 +13,51 @@ if (localStorage.getItem('user-token') == null) {
 // DOM Elements
 const usernameHeading = document.getElementById('username-heading');
 const logoutButton = document.getElementById('logout');
-const addMangaButton = document.getElementById('add-manga');
+const openModalElems = document.querySelectorAll("[data-open]");
+const modalIsVisible = "is-visible";
+const closeModalElems = document.querySelectorAll("[data-close]");
 
 // Functions
 function addManga(e) {
     console.log(e);
 }
 
+
 // Event Handlers
 if (logoutButton) {
     logoutButton.addEventListener('click', onLogout);
 }
 
-addMangaButton.addEventListener('click', addManga);
+// Modal Events
+// Open Modal
+for(const elem of openModalElems) {
+    elem.addEventListener("click", function() {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(modalIsVisible);
+    });
+}
+
+// Close Modal Button
+for (const elem of closeModalElems) {
+    elem.addEventListener("click", function() {
+      this.parentElement.parentElement.parentElement.classList.remove(modalIsVisible);
+    });
+}
+
+// Clicking Outside Modal
+document.addEventListener("click", e => {
+    if (e.target == document.querySelector(".modal.is-visible")) {
+      document.querySelector(".modal.is-visible").classList.remove(modalIsVisible);
+    }
+});
+
+// Pressing Esc To Close Modal
+const isVisible = "is-visible";
+ 
+document.addEventListener("keyup", e => {
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(modalIsVisible);
+  }
+});
+
 
