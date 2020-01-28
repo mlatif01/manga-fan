3949bd0f5d16b962dab8e052cdc839e3b3b42718
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-01-28 16:39:41 +0000

    manga eden api retrives manga images and displays to readmanga page

--- dist/js/manga-eden-service.js
+++ dist/js/manga-eden-service.js
@@ -1,7 +1,6 @@
 // HTTP Request Methods for Manga Eden Api
 var baseMangaListURL = 'https://www.mangaeden.com/api/list/0';
 var baseURL = 'http://localhost:3000/api/';
-var baseMangaImgURL = 'https://cdn.mangaeden.com/mangasimg/';
 var imgArr = [];
 
 // Fetch Manga List only once, then set ID of manga in local storage
@@ -17,10 +16,8 @@ async function getMangaEdenChapter(title, chapter) {
     // Fetch manga list
     const response = await fetch(baseURL+'mangaeden'+`/${title},${chapter}`, options);
     const data = await response.json();
-
-    // push image data from chapters in an array so we can view the manga
-    data.images.forEach(element => {
-        imgArr.push(element[1]);
-    });
+    
+    console.log("received manga chapter data");
+    return data;
 
 }
--- dist/js/read-manga.js
+++ dist/js/read-manga.js
@@ -6,24 +6,43 @@ const mangaInfo = {
     chapter: queryParams.chapter
 }
 
-const mangaChapter = [];
+// images will be from right to left
+const chapterImgArr = [];
+var baseMangaImgURL = 'https://cdn.mangaeden.com/mangasimg/';
 
 // DOM Elements
 const mangaHeaderH1 = document.getElementById('manga-header-heading');
+const mangaImg = document.getElementById('manga-img');
 
 // Authorisation to allow user to access certain resources
 // Check if local storage has user details (temp)
-if (localStorage.getItem('user-token') == null) {
+async function startup() {
+    if (localStorage.getItem('user-token') == null) {
     window.location.replace('index.html');
-// Set up the page
-} else {
-    // Display Manga Name
-    mangaHeaderH1.innerHTML = `${mangaInfo.title} - Chapter ${mangaInfo.chapter} / p1`
+    // Set up the page
+    } else {
+        // Display Manga Name
+        mangaHeaderH1.innerHTML = `${mangaInfo.title} - Chapter ${mangaInfo.chapter} / p1`
 
-    // Retrieve Manga Chapter
-    getMangaEdenChapter(mangaInfo.title, mangaInfo.chapter);
+        // Retrieve Manga Chapter
+        const chapterData = await getMangaEdenChapter(mangaInfo.title, mangaInfo.chapter);
+        
+        // push image data from chapters in an array so we can view the manga
+        chapterData.images.forEach(element => {
+            chapterImgArr.push(element[1]);
+        });
+
+        // reverse arr for easier navigation
+        chapterImgArr.reverse();
+
+        // display first page of the manga
+        mangaImg.setAttribute("src", baseMangaImgURL + chapterImgArr[0]);
+
+    }
 }
 
 // Functions
 
 // Event Handlers
+
+startup();
