59230231075bcda14a19fc2cfdff5323f685c80b
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-02-09 13:48:01 +0000

    change baseurl

--- public/js/user-service.js
+++ public/js/user-service.js
@@ -1,6 +1,6 @@
 // HTTP Request Methods for User
-// var baseURL = 'http://localhost:3000/api/';
-var baseURL = "api/";
+var baseURL = "http://localhost:3000/api/";
+// var baseURL = "api/";
 
 // Functions
 async function getUserData() {
